51c5a584f919be69cdd5610473a81a13d72848e5
cjoelrun <cameronjlopez@gmail.com>
2013-10-01 13:48:48 -0500

    cleaning

--- login.py
+++ login.py
@@ -35,7 +35,7 @@ def parse_config(path):
     except IOError:
         print "Cannot open configuration file."
         os._exit(0)
-    
+
     config = configparser.RawConfigParser()
     config.read(path)
 
@@ -44,28 +44,27 @@ def parse_config(path):
 def parse_node(raw):
     host = raw[0]['Instance_Data']['host']
     node = host[2:].replace('-', '.')
-    
+
     return node
 
 def search_ohthree(ohthree, regions, UUID):
-    for i, e in enumerate(regions):
-        url =  ohthree % (regions[i].strip().lower(), UUID)
+    for i, region in enumerate(regions):
+        url =  ohthree + '/%s/instance/%s/json' % (region.strip().lower(),
+                                                   UUID)
         response = requests.get(url, verify = False)
         if response.status_code == requests.codes.ok:
-            json = response.json()
-        else:
-            continue
-    try:
-        return json
-    except:
-        print "Unable to return JSON; Verify the UUID and ohthree URL."
-        os._exit(0)
+            return response.json()
+    print "Unable to return JSON; Verify the UUID and ohthree URL."
+    os._exit(0)
 
 if __name__ == '__main__':
     args = docopt(__doc__, help = True)
-    
+
     values = parse_config(os.path.expanduser(args['--config']))
-    json = search_ohthree(values['ohthree'].strip('\''), values['regions'].split(','), args['<UUID>'])
+    json = search_ohthree(values['ohthree'].strip('\''),
+                          values['regions'].split(','),
+                          args['<UUID>'])
     node = parse_node(json)
 
-    login(values['bastion'].strip('\''), values['username'], values['password'], node)
+    login(values['bastion'].strip('\''), values['username'],
+          values['password'], node)
